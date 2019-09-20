026de971703cbb980fd5f54c47387baf28ab3bb3
Thatphon Chutinan <newclaffly40685@gmail.com>
2019-09-20 13:53:40 +0700

    Version 1.0

---
+++ SyncDogSystem_main/Agent2/Backup/Agent2/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Agent2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.clone = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.alert = new System.Windows.Forms.Label();
+            this.pg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // clone
+            // 
+            this.clone.BackColor = System.Drawing.Color.DodgerBlue;
+            this.clone.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.clone.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.clone.Location = new System.Drawing.Point(184, 199);
+            this.clone.Name = "clone";
+            this.clone.Size = new System.Drawing.Size(254, 42);
+            this.clone.TabIndex = 0;
+            this.clone.Text = "Force Sync";
+            this.clone.UseVisualStyleBackColor = false;
+            this.clone.Click += new System.EventHandler(this.clone_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(160, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 31);
+            this.label1.TabIndex = 1;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // alert
+            // 
+            this.alert.AutoSize = true;
+            this.alert.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.alert.Location = new System.Drawing.Point(243, 136);
+            this.alert.Name = "alert";
+            this.alert.Size = new System.Drawing.Size(0, 24);
+            this.alert.TabIndex = 2;
+            // 
+            // pg
+            // 
+            this.pg.AutoSize = true;
+            this.pg.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pg.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pg.ForeColor = System.Drawing.Color.Red;
+            this.pg.Location = new System.Drawing.Point(281, 265);
+            this.pg.Name = "pg";
+            this.pg.Size = new System.Drawing.Size(289, 13);
+            this.pg.TabIndex = 3;
+            this.pg.Text = "หมายเหตุ : ระบบจะทำการซิงค์ข้อมูลเวลา 07:30:00 ของทุกวัน ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(582, 287);
+            this.Controls.Add(this.pg);
+            this.Controls.Add(this.alert);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.clone);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SyncSystemDog";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button clone;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label alert;
+        private System.Windows.Forms.Label pg;
+    }
+}
+
---
+++ SyncDogSystem_main/Agent2/Backup/Agent2/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace Agent2
+{
+    public partial class Form1 : Form
+    {
+        string connecting = @"Data Source=172.16.16.1;Initial Catalog=bds_pp_srct;Integrated Security=true;"; //sql connection
+
+        string partnumber;
+        string srctcode;
+        string dockcode;
+        string pack;
+        string error;
+        string chk;
+        string user;
+        string ekb;
+        string kbid;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void select_hanheld()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connecting))
+                {
+                    SqlDataAdapter dtaddAdapter;
+                    DataTable dtselect = new DataTable();
+                    string select = "SELECT Part_Number,SRCT_Code,Dock_Code,Package,Error_Code,Chk_Type,LogUser,ekb_order_no,Kanban_ID FROM Hanheld";
+                    dtaddAdapter = new SqlDataAdapter(select, conn);
+                    dtaddAdapter.Fill(dtselect);
+                    conn.Close();
+                    for (int i = 0; i < dtselect.Rows.Count; i++)
+                    {
+                        partnumber = dtselect.Rows[i]["Part_number"].ToString();
+                        srctcode = dtselect.Rows[i]["SRCT_Code"].ToString();
+                        dockcode = dtselect.Rows[i]["Dock_Code"].ToString();
+                        pack = dtselect.Rows[i]["Package"].ToString();
+                        error = dtselect.Rows[i]["Error_Code"].ToString();
+                        chk = dtselect.Rows[i]["Chk_Type"].ToString();
+                        user = dtselect.Rows[i]["LogUser"].ToString();
+                        //day = dtselect.Rows[i]["LogDate"].ToString();
+                        ekb = dtselect.Rows[i]["ekb_order_no"].ToString();
+                        kbid = dtselect.Rows[i]["Kanban_ID"].ToString();
+                        insert_newtable();
+
+                    }
+                    // MessageBox.Show("Clone Succees");
+                    delete_log_hanheld();
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void insert_newtable()
+        {
+            string day = DateTime.Now.ToString("dd/mm/yy hh:mm:ss tt");
+            using (SqlConnection conn = new SqlConnection(connecting))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = @"INSERT  INTO report_hanheld(Part_Number,SRCT_Code,Dock_Code,Package,Error_Code,Chk_Type,LogUser,LogDate,ekb_order_no,Kanban_ID) VALUES(@partnumber,@srctcode,@dockcode,@pack,@error,@chk,@user,@day,@ekb,@kbid)";
+                    cmd.Parameters.AddWithValue("@partnumber", partnumber);
+                    cmd.Parameters.AddWithValue("@srctcode", srctcode);
+                    cmd.Parameters.AddWithValue("@dockcode", dockcode);
+                    cmd.Parameters.AddWithValue("@pack", pack);
+                    cmd.Parameters.AddWithValue("@error", error);
+                    cmd.Parameters.AddWithValue("@chk", chk);
+                    cmd.Parameters.AddWithValue("@user", user);
+                    cmd.Parameters.AddWithValue("@day", day);
+                    cmd.Parameters.AddWithValue("@ekb", ekb);
+                    cmd.Parameters.AddWithValue("@kbid", kbid);
+                    try
+                    {
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (SqlException e)
+                    {
+                        // MessgeBox.Show(e.Message.ToString(), "Error Message");
+                    }
+                }
+            }
+        }
+
+        private void delete_log_hanheld()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connecting))
+                {
+                    SqlDataAdapter dtaddAdapter;
+                    DataTable dtselect = new DataTable();
+                    string select = "DELETE FROM Hanheld";
+                    dtaddAdapter = new SqlDataAdapter(select, conn);
+                    dtaddAdapter.Fill(dtselect);
+                    conn.Close();
+                    //MessageBox.Show("Delete Succees");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+            string hh = DateTime.Now.ToString("hh");
+            string mm = DateTime.Now.ToString("mm");
+            string ss = DateTime.Now.ToString("ss");
+            if (hh == "7" && mm == "30" && ss == "00")
+            {
+                select_hanheld();
+                alert.Text = "ทำการซิงค์ข้อมูล";
+                //MessageBox.Show("Complete");
+            }
+            else
+            {
+                alert.Text = "ไม่มีการซิงค์ข้อมูล";
+            }
+        }
+
+        private void clone_Click(object sender, EventArgs e)
+        {
+            select_hanheld();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString("dd/mm/yy hh:mm:ss tt");
+        }
+
+       
+
+
+    }
+}
